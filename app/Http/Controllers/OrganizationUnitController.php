185a292526741c7afd2c68adf48a16f31a34b033
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-06-07 22:02:55 +0700

    Prepare models and migrations for OrganizationUnit and Employee

---
+++ app/Http/Controllers/OrganizationUnitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrganizationUnit;
+use App\Http\Requests\StoreOrganizationUnitRequest;
+use App\Http\Requests\UpdateOrganizationUnitRequest;
+
+class OrganizationUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrganizationUnitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrganizationUnit $organizationChart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrganizationUnit $organizationChart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOrganizationUnitRequest $request, OrganizationChart $organizationChart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrganizationUnit $organizationChart)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_06_07_142246_create_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('start_date')->comment('job start date');
+            $table->timestamp('stop_date')->nullable()->comment('ex. job resigned date');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
