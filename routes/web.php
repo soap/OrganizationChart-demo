7eaa54bd77cb91fdd2d093a25adb3f692aa2bea6
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-07-19 00:50:21 +0700

    Authentication related test cases are working in progress as well as Profile test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,9 @@ Auth::routes(['verify' => true]);
 
 Route::middleware(['auth'])->group(function() { 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/unit/{orgUnit?}', App\Http\Controllers\DisplayUnitController::class)->name('unit.show');
 });
 
