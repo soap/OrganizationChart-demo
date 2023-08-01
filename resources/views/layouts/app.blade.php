00c35dcb6da82bbcbd8fa9b2c9f0592e65b5f176
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2023-08-01 11:02:09 +0700

    Add links to application layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('unit.show') }}">{{ __('Unit') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('unit.liveshow') }}">{{ __('Live Unit') }}</a>
+                            </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
